423093f1017333bba4ec36cbcf45c10b56a4e3ca
Chao Chen <chaochen@live.com>
2024-05-25 15:30:29 -0400

    create cart reducer and replace cart context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom';
-import { useContext } from 'react';
+import { useSelector } from 'react-redux';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
-import { useSelector } from 'react-redux';
-
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
-import { CartContext } from '../../contexts/cart.context';
+import { selectCurrentUser } from '../../store/user/user.selector';
+import { selectIsCartOpen } from '../../store/cart/cart.selector';
 
 import {
   NavigationContainer,
@@ -15,11 +14,10 @@ import {
   NavLinks,
   NavLink,
 } from './navigation.styles';
-import { selectCurrentUser } from '../../store/user/user.selector';
 
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser);
-  const { isCartOpen } = useContext(CartContext);
+  const isCartOpen = useSelector(selectIsCartOpen);
 
   return (
     <>
